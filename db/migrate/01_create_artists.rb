9125eaa3d6d17cc4cf74d0faec625c7dbe9d7efb
github username <github email address>
2018-10-19 14:31:54 +0100

    Done.

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,16 @@
+class CreateArtists < ActiveRecord::Migration
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+end
+
+
+connection = ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
